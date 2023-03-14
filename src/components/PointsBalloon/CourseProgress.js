6f97652da408966437ef8d68678eed89fcdbc9fd
redande <antti.leinonen@helsinki.fi>
2023-03-14 16:34:10 +0200

    Improve points visualization

--- src/components/PointsBalloon/CourseProgress.js
+++ src/components/PointsBalloon/CourseProgress.js
@@ -2,20 +2,22 @@ import React from "react"
 import PartProgress from "./PartProgress"
 
 const CourseProgress = ({ data, appliesForStudyRight }) => {
-  return (
-    data && (
-      <div>
-        {Object.entries(data).map(([name, data]) => {
-          return (
-            <PartProgress
-              appliesForStudyRight={appliesForStudyRight}
-              name={name}
-              data={data}
-            />
-          )
-        })}
-      </div>
-    )
+  return data && Object.entries(data).length === 0 ? (
+    <div>
+      Edistymisesi näkyy täällä kunhan olet tehnyt ensimmäisen tehtäväsi
+    </div>
+  ) : (
+    <div>
+      {Object.entries(data).map(([name, data]) => {
+        return (
+          <PartProgress
+            appliesForStudyRight={appliesForStudyRight}
+            name={name}
+            data={data}
+          />
+        )
+      })}
+    </div>
   )
 }
 
